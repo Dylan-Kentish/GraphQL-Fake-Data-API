3d47d7245113b9c5638991a909d456f0a7dd6754
Dylan Kentish <dylankentish@live.co.uk>
2022-09-01 14:04:20 +0100

    Update users_test.go
    Added test for get all users

--- api/users_test.go
+++ api/users_test.go
@@ -1,15 +1,16 @@
 package api_test
 
 import (
-	"encoding/json"
 	"fmt"
 	"strconv"
 	"testing"
 
 	"github.com/Dylan-Kentish/GraphQLFakeDataAPI/api"
+	"github.com/Dylan-Kentish/GraphQLFakeDataAPI/utils"
 	"github.com/graphql-go/graphql"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"golang.org/x/exp/maps"
 )
 
 func TestScehema(t *testing.T) {
@@ -26,9 +27,8 @@ var _ = Describe("Users", func() {
 		Expect(r.Errors).To(BeEmpty())
 
 		result := r.Data.(map[string]interface{})
-		userJSON, _ := json.Marshal(result["user"])
 		var user api.User
-		json.Unmarshal(userJSON, &user)
+		utils.As(result["user"], &user)
 
 		Expect(user).To(Equal(api.User{}))
 	})
@@ -40,7 +40,7 @@ var _ = Describe("Users", func() {
 		userTests[i] = Entry(user.ID, id)
 	}
 
-	DescribeTable("Responds to query", func(id int) {
+	DescribeTable("Get user by ID", func(id int) {
 		// Query
 		query := fmt.Sprintf(`{user(id:"%s"){id,name,username}}`, fmt.Sprint(id))
 		params := graphql.Params{Schema: api.UserSchema, RequestString: query}
@@ -48,10 +48,23 @@ var _ = Describe("Users", func() {
 		Expect(r.Errors).To(BeEmpty())
 
 		result := r.Data.(map[string]interface{})
-		userJSON, _ := json.Marshal(result["user"])
 		var user api.User
-		json.Unmarshal(userJSON, &user)
+		utils.As(result["user"], &user)
 
 		Expect(user).To(Equal(api.UserData[id]))
 	}, userTests)
+
+	It("Get all users", func() {
+		// Query
+		query := `{users{id,name,username}}`
+		params := graphql.Params{Schema: api.UserSchema, RequestString: query}
+		r := graphql.Do(params)
+		Expect(r.Errors).To(BeEmpty())
+
+		result := r.Data.(map[string]interface{})
+		var users []api.User
+		utils.As(result["users"], &users)
+
+		Expect(users).To(ContainElements(maps.Values(api.UserData)))
+	})
 })
