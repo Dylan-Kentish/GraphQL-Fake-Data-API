ae63f2803a49e6d9cde49ed49818c26dbc5ab6da
Dylan Kentish <dylankentish@live.co.uk>
2022-09-01 10:49:54 +0100

    Extracted schema into separate file

---
+++ internal/schema.go
@@ -0,0 +1,17 @@
+package internal
+
+import (
+	"github.com/graphql-go/graphql"
+)
+
+var fields = graphql.Fields{
+	"hello": &graphql.Field{
+		Type: graphql.String,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return "world", nil
+		},
+	},
+}
+var rootQuery = graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+var schemaConfig = graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+var Schema, _ = graphql.NewSchema(schemaConfig)
--- main.go
+++ main.go
@@ -1,35 +1,21 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
-	"github.com/graphql-go/graphql"
+	api "github.com/Dylan-Kentish/GraphQLFakeDataAPI/internal"
 	"github.com/graphql-go/handler"
 )
 
 func main() {
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
-
 	h := handler.New(&handler.Config{
-		Schema:   &schema,
+		Schema:   &api.Schema,
 		Pretty:   true,
 		GraphiQL: true,
 	})
 
+	fmt.Println("Starting server at localhost:8080/graphql")
 	http.Handle("/graphql", h)
 	http.ListenAndServe(":8080", nil)
 }
