dd6f6b5e172e04a65e94c335112a4900f979d7ba
Kent <dylankentish@live.co.uk>
2022-09-03 12:15:41 +0100

    Adding missing test case for new photos field on Album

--- api/albums_test.go
+++ api/albums_test.go
@@ -161,6 +161,7 @@ var _ = Describe("Albums", func() {
 		},
 			Entry("id", "id"),
 			Entry("userid", "userid"),
-			Entry("description", "description"))
+			Entry("description", "description"),
+			Entry("photos", "photos{id}"))
 	})
 })
